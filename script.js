7306fbb4e213901c009044ae65526f63a923c737
Joe Plummer <jmplumm97@gmail.com>
2026-06-07 19:31:22 -0400

    Changed index.html form so that submit doesn't actually 'submit' and reload the page, so it's now just a button, also added more comments and comment header. Edited style.css to add comments and comment header. Edited script.js so that 'Hello' is printed to console when Submit button is pressed.

---
+++ script.js
@@ -0,0 +1,20 @@
+/**
+ * File: script.css
+ * GUI Assignment: HW3 - Creating an Interactive Dynamic Table
+ * Description: JavaScript file for Assignment 3
+ * Joe Plummer, UMass Lowell Computer Science, joseph_plummer@student.uml.edu
+ * Copyright (c) 2026 by Joe Plummer. All rights reserved. May be freely copied or excerpted for educational purposes with credit to the author.
+ */
+
+console.log("Starting...");
+
+
+
+document.getElementById("submit-btn").addEventListener('click', function() {
+    console.log("Hello");
+});
+
+// console.log(num);
+console.log("Ending...");
+
+// document.write(minColNum)
