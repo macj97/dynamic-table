fc28a0f39a7fe5ed8832892acd7fe6d5bb8a34c9
Joe Plummer <jmplumm97@gmail.com>
2026-06-08 09:16:06 -0400

    Edited all index.html, script.js, and style.css. Cleaned up and added/fixed comments in all files.

--- script.js
+++ script.js
@@ -6,21 +6,16 @@
  * Copyright (c) 2026 by Joe Plummer. All rights reserved. May be freely copied or excerpted for educational purposes with credit to the author.
  */
 
+
+// variables
+let minColNum, maxColNum, minRowNum, maxRowNum; // min and max numbers for the columns and rows
+let table_container = document.getElementById("Table-container"); // container of the table element
+
 /**
- * Still need to check if input is a number and not a string,
- * and need to check if min is less than max.
- * Need table to be scrollable if bigger than the screen,
- * with the first column and row fixed.
+ * Function checks to see if a number is within a certain bounds
+ * @param   x    int    converted to an int to compare against bounds
+ * @return  bool
  */
-
-console.log("Starting...");
-
-// let elements;
-let minColNum, maxColNum, minRowNum, maxRowNum;
-let minColNum_e = false, maxColNum_e = false, 
-    minRowNum_e = false, maxRowNum_e = false;
-let table_container = document.getElementById("Table-container");
-
 function withinBounds(x) {
     x = Number(x);
     if (x < -50 || x > 50) {
@@ -31,22 +26,28 @@ function withinBounds(x) {
     }
 }
 
+/**
+ * Function that checks to see if one num is less than or equal to the other
+ * @param   x   int     converted to int, minimum number
+ * @param   y   int     converted to int, maximum number
+ * @return  bool
+ */
 function isMinLEMax(x, y) {
     x = Number(x);
     y = Number(y);
-    console.log("x = ", x, ", y = ", y);
+    // console.log("x = ", x, ", y = ", y); // debugging
     if (x <= y) {
-        console.log("Returning true...");
+        // console.log("Returning true..."); // debugging
         return true;
     }
     else {
-        console.log("Returning false...");
+        // console.log("Returning false..."); // debugging
         return false;
     }
 }
 
 /**
- * Function creates an element that shows an error message
+ * Function creates an element that shows an error message, also clears the dynamic table
  * @param   elem    element    element before the error element is being placed
  * @param   e       int        0-error message for bounds and non-number input, 
  *                             1-error message for min and max
@@ -67,11 +68,14 @@ function createErrorElement(elem, e) {
     table_container.innerHTML = "";
 }
 
-
+/**
+ * Function that sets the display property of "error" elements off
+ * @param   N/A    
+ * @return  N/A
+ */
 function turnOffErrorMessages() {
-    // turn all error messages off
     errors = document.getElementsByClassName("error");
-    console.log(errors);
+    // console.log(errors); // debugging
     if (errors.length >= 1) {
         for (let k = 0; k < errors.length; k++) {
             errors[k].style.display = "none";
@@ -81,11 +85,10 @@ function turnOffErrorMessages() {
 
 // Event listener for when Submit button is pressed
 document.getElementById("submit-btn").addEventListener('click', function() {
-    console.log("Hello");
 
     // getting the values from the form (with error handling)
     minColNum = document.getElementById("mincolval").value;
-    console.log(minColNum);
+    // console.log(minColNum); // debugging
     if (!Number.isInteger(Number(minColNum))) { // if not a number
         console.log("wasn't a number...");
         turnOffErrorMessages();
@@ -103,17 +106,13 @@ document.getElementById("submit-btn").addEventListener('click', function() {
     }
 
     maxColNum = document.getElementById("maxcolval").value;
-    console.log(maxColNum);
+    // console.log(maxColNum); // debugging
     if (!Number.isInteger(Number(maxColNum))) { // if not a number
         turnOffErrorMessages();
         createErrorElement(document.getElementById("maxcolval"), 0);
         return;
     }
     if (!withinBounds(maxColNum)) { // if not within bounds
-        // if (!maxColNum_e) {
-        //     createErrorElement(document.getElementById("maxcolval"), 0);
-        //     maxColNum_e = true;
-        // }
         turnOffErrorMessages();
         createErrorElement(document.getElementById("maxcolval"), 0);
         return;
@@ -125,34 +124,26 @@ document.getElementById("submit-btn").addEventListener('click', function() {
     }
 
     minRowNum = document.getElementById("minrowval").value;
-    console.log(minRowNum);
+    // console.log(minRowNum); // debugging
     if (!Number.isInteger(Number(minRowNum))) { // if not a number
         turnOffErrorMessages();
         createErrorElement(document.getElementById("minrowval"), 0);
         return;
     }
     if (!withinBounds(minRowNum)) {
-        // if (!minRowNum_e) {
-        //     createErrorElement(document.getElementById("minrowval"));
-        //     minRowNum_e = true;
-        // }
         turnOffErrorMessages();
         createErrorElement(document.getElementById("minrowval"), 0);
         return;
     }
 
     maxRowNum = document.getElementById("maxrowval").value;
-    console.log(maxRowNum);
+    // console.log(maxRowNum); // debugging
     if (!Number.isInteger(Number(maxRowNum))) { // if not a number
         turnOffErrorMessages();
         createErrorElement(document.getElementById("maxrowval"), 0);
         return;
     }
     if (!withinBounds(maxRowNum)) {
-        // if (!maxRowNum_e) {
-        //     createErrorElement(document.getElementById("maxrowval"));
-        //     maxRowNum_e = true;
-        // }
         turnOffErrorMessages();
         createErrorElement(document.getElementById("maxrowval"), 0);
         return;
@@ -164,16 +155,14 @@ document.getElementById("submit-btn").addEventListener('click', function() {
     }
 
     turnOffErrorMessages();
-
     
     // building the table
-    console.log(table_container);
+    // console.log(table_container);  // debugging
     table_container.innerHTML = ""
     const dTable = document.createElement("table");
 
     // first row:
     const headerRow = document.createElement("tr");
-    // headerRow.className = "header-row";
     const hiddenCell = document.createElement("th");
     hiddenCell.className = "hide-cell";
     headerRow.appendChild(hiddenCell);
@@ -204,7 +193,3 @@ document.getElementById("submit-btn").addEventListener('click', function() {
     }
     table_container.appendChild(dTable);
 });
-
-
-console.log(table);
-console.log("Ending...");
